caed887b84cf4f640d330a2b1d8ff97c823812f9
BlackPandalancer <blackpandalancer@gmail.com>
2020-03-28 15:34:03 -0500

    Use Java enum to handle user roles in all application by adding new UserRole enum in dto package.

--- onlineshopping/src/main/java/net/hka/onlineshopping/handler/RegisterHandler.java
+++ onlineshopping/src/main/java/net/hka/onlineshopping/handler/RegisterHandler.java
@@ -106,6 +106,7 @@ public class RegisterHandler {
 		return transitionValue;
 	}
 	
+	// return an array for user roles for only role user and role supplier
 	public UserRole[] populateRoles() {
         return new UserRole[] { UserRole.USER, UserRole.SUPPLIER };
     }
--- shoppingbackend/src/main/java/net/hka/shoppingbackend/dto/UserRole.java
+++ shoppingbackend/src/main/java/net/hka/shoppingbackend/dto/UserRole.java
@@ -2,17 +2,19 @@ package net.hka.shoppingbackend.dto;
 
 public enum UserRole {
 	USER("USER"), SUPPLIER("SUPPLIER"), ADMIN("ADMIN");
-	
-	private String value;	
-	
+
+	private String value;
+
 	private UserRole(String value) {
 		this.value = value;
 	}
-	
+
 	public String getValue() {
 		return this.value;
 	}
 
+	// return a label formated string by capitalize the first character and make
+	// rest of the role word as lower case
 	public String getValueAsLabel() {
 		return Character.toUpperCase(this.value.charAt(0)) + this.value.toLowerCase().substring(1);
 	}
